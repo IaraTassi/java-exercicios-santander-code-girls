8fde47d61f6b4110f83ae33f68b5253abeb48771
Iara Tassi <iaratassi@hotmail.com>
2023-06-30 21:18:30 -0300

    Exercício Quadrilátero

--- java-exercicios/src/br/com/dio/Main.java
+++ java-exercicios/src/br/com/dio/Main.java
@@ -3,20 +3,27 @@ package br.com.dio;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Exercicio calculadora");
+        System.out.println("\n Exercicio calculadora");
 
-        Calculadora.somaNumero(8, 3);
+        Calculadora.somaNumero(8,3);
         Calculadora.subtrairNumero(7, 5);
         Calculadora.multiplicaNumero(6, 4);
         Calculadora.dividirNumero(9, 2.3);
 
-        System.out.println("Exercício mensagem");
+        System.out.println("\n Exercício mensagem");
         Mensagem.horaDiaMensagem(6);
         Mensagem.horaDiaMensagem(14);
         Mensagem.horaDiaMensagem(23);
 
-        System.out.println("Exercício Empréstimo");
+        System.out.println("\n Exercício empréstimo");
         Emprestimo.valorCalculado(1000, Emprestimo.getDuasParcelas());
         Emprestimo.valorCalculado(1000, Emprestimo.getTresParcelas());
+
+        System.out.println("\n Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+
     }
 }
