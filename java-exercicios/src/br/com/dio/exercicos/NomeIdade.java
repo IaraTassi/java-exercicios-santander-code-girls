37e03b4a022a5270cb5ab4e0d0aa9ebcbdf6194e
Iara Tassi <iaratassi@hotmail.com>
2023-07-01 22:55:58 -0300

    Exercício Estruturas de repetição while

---
+++ java-exercicios/src/br/com/dio/exercicos/NomeIdade.java
@@ -0,0 +1,29 @@
+package br.com.dio.exercicos;
+
+
+import java.util.Scanner;
+
+/* Faça um programa que leia conjunto de dois valores, o primeiro
+representando o nome do aluno e o segundo representando sua idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+ */
+public class NomeIdade {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui...");
+    }
+
+}
