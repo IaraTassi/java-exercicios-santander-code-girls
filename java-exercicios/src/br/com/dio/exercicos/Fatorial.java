bae44ce96e0fb8ad8ece307d9526324611c9f7c5
Iara Tassi <iaratassi@hotmail.com>
2023-07-02 22:06:29 -0300

    Exercício Estruturas de repetição do for

---
+++ java-exercicios/src/br/com/dio/exercicos/Fatorial.java
@@ -0,0 +1,27 @@
+package br.com.dio.exercicos;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo
+usuário. Exemplo 5!= 120 (5 x 4 x 3 x 2 x 1)
+ */
+public class Fatorial {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial +"! = ");
+        for (int i = fatorial; i >= 1; i--) {
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+    }
+
+}
---
+++ java-exercicios/src/br/com/dio/exercicos/Tabuada.java
@@ -0,0 +1,32 @@
+package br.com.dio.exercicos;
+
+import java.util.Scanner;
+
+/*
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de qualquer número
+inteiro entre 1 a 10. O usuário deve informar de qual número ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+Tabuada de 5:
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+....
+5 x 10 = 50
+
+ */
+public class Tabuada {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Tabuada: ");
+        int tabuada = scan.nextInt();
+
+        System.out.println("Tabuada de: " + tabuada);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(tabuada + " X " + i + " = " + (tabuada * i));
+        }
+    }
+}
