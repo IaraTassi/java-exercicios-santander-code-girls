6bd98ebc617b23fac63a5b4c9ac5ad0a6660bf33
Iara Tassi <iaratassi@hotmail.com>
2023-07-04 22:23:45 -0300

    Exercícios proposto5

---
+++ java-exercicios/src/br/com/dio/collection/ExercicioProposto5.java
@@ -0,0 +1,44 @@
+package br.com.dio.collection;
+
+import java.util.*;
+
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi inserido.
+ */
+public class ExercicioProposto5 {
+    public static void main(String[] args) {
+
+        int quantidadeLancamentos = 100;
+
+        List<Integer> valores = new ArrayList<>();
+        Random geraLancamento = new Random();
+        for (int i = 0; i < quantidadeLancamentos; i++) {
+            int numero = geraLancamento.nextInt(6) + 1;
+            valores.add(numero);
+        }
+        Map<Integer, Integer> lancamentos = new HashMap<>();
+        for (Integer resultado : valores) {
+            if (lancamentos.containsKey(resultado))
+                lancamentos.put(resultado, (lancamentos.get(resultado) + 1));
+            else lancamentos.put(resultado, 1);
+
+        }
+
+        System.out.print("Jogando");
+         for (int i = 0; i < 3; i++) {
+            try {
+                Thread.sleep(3000);
+                System.out.print(".");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.print("Jogando");
+        System.out.println("\nValor: " + "Quantidade de vezes: ");
+        for (Map.Entry<Integer, Integer> entry : lancamentos.entrySet()) {
+            System.out.printf("%3d\n %10d\n", entry.getKey(), entry.getValue());
+        }
+    }
+
+}
